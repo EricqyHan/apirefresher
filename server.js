4ab83925d6e5679dc65214eb5384471066a9e8d9
EricQyhan <Ericqyhan@gmail.com>
2024-04-28 14:28:38 -0400

    fixed cors errors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express"); // make sure server has access to express
 const app = express(); // this will hold express for us
 const PORT = 8000;
+const cors = require("cors");
+
+app.use(cors());
 
 const rappers = {
   "21 savage": {
