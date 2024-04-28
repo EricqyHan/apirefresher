9ec5dc986db6cfb2240d7b5a29a7d0252631d10c
EricQyhan <Ericqyhan@gmail.com>
2024-04-28 13:32:06 -0400

    heroku 3

--- server.js
+++ server.js
@@ -1,53 +1,43 @@
-// make sure server has access to express
-const express = require("express");
-// this will hold express for us
-const app = express();
-const PORT = 8888;
+const express = require("express"); // make sure server has access to express
+const app = express(); // this will hold express for us
+const PORT = 8000;
 
 const rappers = {
   "21 savage": {
-    age: 31,
+    age: 29,
     birthName: "Shéyaa Bin Abraham-Joseph",
-    birthLocation: "Plaistow, London, England",
+    birthLocation: "London, England",
   },
   "chance the rapper": {
-    age: 31,
-    birthName: "Chancelor Johnathan Bennett",
-    birthLocation: "Chicago, Illinois, US",
-  },
-  "rich brian": {
-    age: 24,
-    birthName: "Brian Imanuel Soewarno",
-    birthLocation: "Jakarta, Indonesia",
+    age: 29,
+    birthName: "Chancelor Bennett",
+    birthLocation: "Chicago, Illinois",
   },
   dylan: {
-    age: 100,
+    age: 29,
     birthName: "Dylan",
     birthLocation: "Dylan",
   },
 };
 
-// set up server to hear express and generate response with a HTML file. Note that this will be a GET request
-// and then we send a file with response.sendFile()
+// we now have our route
+// request and response are just param names but it is good pracrice to name them that
 app.get("/", (request, response) => {
+  // __dirname is wherever the file is that is where we start looking for those files
   response.sendFile(__dirname + "/index.html");
 });
 
 app.get("/api/:rapperName", (request, response) => {
-  const rappersName = request.params.rapperName.toLowerCase();
-  console.log(request.params.rapperName);
+  // console.log(request.params.rapperName);
   // [] is bracket notation and enables to grab the key, especially with dot notation
+  const rappersName = request.params.rapperName.toLowerCase();
   if (rappers[rappersName]) {
     response.json(rappers[rappersName]);
   } else {
     response.json(rappers["dylan"]);
   }
-  //   response.json(rappers);
 });
 
-// telling our server to listen to port number
-app.listen(process.env.port, () => {
-  console.log(
-    `The server is running on port ${process.env.port}! You better go catch it!`
-  );
+app.listen(process.env.port || PORT, () => {
+  console.log(`The server is running on port ${PORT}! You better go catch it!`);
 });
